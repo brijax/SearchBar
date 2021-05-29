3a0fac0bb89b02ee6b1215d9c117717e1268307c
Bri Jackson <71437390+brijax@users.noreply.github.com>
2021-05-29 11:04:14 +0100

    update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/BJ/Documents/web_dev/github/SearchBar/src/css/custom.css"),
+];
